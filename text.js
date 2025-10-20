4d82af30e0153ebf2806840c8fccd81d73f4d501
Thanhnguyxnn <thanhnguyentr968@gmail.com>
2025-10-20 19:40:03 +0700

    Update text.js

--- text.js
+++ text.js
@@ -26,7 +26,7 @@ var S = {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
       // 倒计时修改这句中的数字，不需要倒计时删除“|#countdown 3|”，换内容修改文字
-      S.UI.simulate('|#countdown 3|CHÚC|Bạn\niu|20\n10|VUI\nVẺ|❤️');
+      S.UI.simulate('|#countdown 3|CHÚC|Mẹ\niu|20\n10|VUI\nVẺ|❤️');
     }
 
     S.Drawing.loop(function () {
